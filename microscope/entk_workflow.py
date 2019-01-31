04e6c9f30ace7c6793b01352668c2b783e1da7f8
Jumana Dakka <jumanadakka@gmail.com>
2019-01-31 15:47:45 -0500

    update functions

--- microscope/entk_workflow.py
+++ microscope/entk_workflow.py
@@ -5,6 +5,7 @@ import sys
 from glob import glob
 import radical.utils as ru
 import shutil
+from time import sleep
 
 
 # suspend pipelines:  
@@ -22,214 +23,174 @@ port = int(os.environ.get('RMQ_PORT',5672))
 
 logger = ru.Logger(__name__, level='DEBUG')
 
-class md_pipeline(Pipeline):
-    def __init__(self, name):
-        super(md_pipeline, self).__init__()
-        self.name = name 
 
+class MD_pipeline:
 
+    def __init__(self, name = 'MD_pipeline'):
 
-class cvae_pipeline(Pipeline):
-    def __init__(self, name):
-        super(cvae_pipeline, self).__init__()
-        self.name = name 
-
-
-class md_stage(Stage):
-    def __init__(self, name):
-        super(md_stage, self).__init__()
         self.name = name
-        self.post_exec = {
-        'condition': self.func_condition(),
-        'on_true': self.func_on_true(),
-        'on_false': self.func_on_false()
-        }
+        self.engine = None
+        self.system = None
 
-    def func_condition(self):
-
-        self.parent_pipeline.suspend()
-        print 'Suspending pipeline %s for 10 seconds' %self.parent_pipeline.uid
-        sleep(10)
-        return True
-
-    def func_on_true(self):
-
-        print 'Resuming pipeline %s' %self.parent_pipeline.uid
-        self.parent_pipeline.resume()
-
-    def func_on_false(self):
-        pass
-
+        self.processes = None
+        self.gpu_processes = None
+        self.threads_per_process = None
+        self.gpu_threads_per_process = None
+        self.duration = None 
+        self.p = Pipeline()
+        self.p.name = name
         
-class cvae_train_stage(Stage):
-    def __init__(self, name):
-        super(cvae_stage, self).__init__()
-        self.name = name
 
-class cvae_inference_stage(Stage):
-    def __init__(self, name):
-        super(cvae_stage, self).__init__()
-        self.name = name
-        self.post_exec = {
-        'condition': self.func_condition(),
-        'on_true': self.func_on_true(),
-        'on_false': self.func_on_false()
-    }
+    def generate_task(self):
+        
+        task = Task()
+        task.name = 'md_task'
+
+        task.pre_exec = ['export PATH=/home/dakka/stress-ng-0.09.40:$PATH']
+        task.executable = ['export PATH=/home/dakka/stress-ng-0.09.40:$PATH']
+        task.arguments = ['-c', '32', '-t', '{}'.format(self.duration)]
+        task.cpu_reqs = {'processes': self.processes,
+                         'process_type': 'MPI',
+                         'threads_per_process': self.threads_per_process,
+                         'thread_type': None
+                         }
+
+        task.gpu_reqs = {'processes': self.gpu_processes,
+                         'process_type': 'MPI',
+                         'threads_per_process': self.gpu_threads_per_process,
+                         'thread_type': None
+                         }
+
+        return task
+
+    def generate_stage(self):
+        s = Stage()
+        s.name = 'MD_stage'
+        s.add_tasks(self.generate_task()) 
+        s.post_exec = {
+            'condition': self.func_condition(),
+            'on_true': self.func_on_true(),
+            'on_false': self.func_on_false()
+        }
+        return s
 
     def func_condition(self):
 
-        self.parent_pipeline.suspend()
-        print 'Suspending pipeline %s for 10 seconds' %self.parent_pipeline.uid
+        self.p.suspend()
+        print 'Suspending pipeline {} for 10 seconds'.format(self.p.name)  
         sleep(10)
         return True
 
     def func_on_true(self):
 
-        print 'Resuming pipeline %s' %self.parent_pipeline.uid
-        self.parent_pipeline.resume()
+        print 'Resuming pipeline {}'.format(self.p.name) 
+        self.p.resume()
 
     def func_on_false(self):
         pass
 
+    def generate_pipeline(self):
 
+        self.p.add_stages(self.generate_stage())
 
-class md_task(Task):
-    def __init__(self, name, duration):
-        super(md_task, self).__init__()
-        self.name = name   
-        self.pre_exec = ['export PATH=/home/dakka/stress-ng-0.09.40:$PATH']
-        self.executable = ['stress-ng']
-        self.arguments = ['-c', '32', '-t', '{}'.format(duration)]
-        self.cpu_reqs = {'processes': 1, 'thread_type': None, 'threads_per_process': 32, 'process_type': None}  
-        self.gpu_reqs = {'processes': 1}  
-        self.copy_input_data = []
+        return self.p
 
+class CVAE_pipeline:
 
-class cvae_training_task(Task):
-    def __init__(self, name, hyperspace_index):
-        super(cvae_task, self).__init__()
-        self.name = name   
-        self.pre_exec = ['export PATH=/home/dakka/stress-ng-0.09.40:$PATH']
-        self.executable = ['stress-ng']
-        self.arguments = ['-c', '32', '-t', '6000'] 
-        self.cpu_reqs = {'processes': 1, 'thread_type': None, 'threads_per_process': 32, 'process_type': None}
-        self.gpu_reqs = {'processes': 1}
-        self.copy_input_data = []
+    def __init__(self, name = 'CVAE_pipeline'):
 
+        self.name = name
+        self.engine = None
+        self.system = None
 
-class cvae_inference_task(Task):
-    def __init__(self, name):
-        super(cvae_task, self).__init__()
-        self.name = name   
-        self.pre_exec = ['export PATH=/home/dakka/stress-ng-0.09.40:$PATH']
-        self.executable = ['stress-ng']
-        self.arguments = ['-c', '1', '-t', '6000'] 
-        self.cpu_reqs = {'processes': 1, 'thread_type': None, 'threads_per_process': 1, 'process_type': None}
-        self.copy_input_data = []
+        self.processes = None
+        self.gpu_processes = None
+        self.threads_per_process = None
+        self.gpu_threads_per_process = None
+        self.duration = None 
+        self.p = Pipeline()
+        self.p.name = name
+        
+
+    def generate_task(self, task_no):
+        
+        task = Task()
+        task.name = 'cvae_train_task_{}'.format(task_no)
+
+        task.pre_exec = ['export PATH=/home/dakka/stress-ng-0.09.40:$PATH']
+        task.executable = ['export PATH=/home/dakka/stress-ng-0.09.40:$PATH']
+        task.arguments = ['-c', '32', '-t', '{}'.format(self.duration)]
+        task.cpu_reqs = {'processes': self.processes,
+                         'process_type': 'MPI',
+                         'threads_per_process': self.threads_per_process,
+                         'thread_type': None
+                         }
+
+        task.gpu_reqs = {'processes': self.gpu_processes,
+                         'process_type': 'MPI',
+                         'threads_per_process': self.gpu_threads_per_process,
+                         'thread_type': None
+                         }
+
+        return task
+
+    def generate_stage(self):
+        s = Stage()
+        s.name = 'MD_stage'
+        s.add_tasks(self.generate_task(task_no = "i")) 
+        s.post_exec = {
+            'condition': self.func_condition(),
+            'on_true': self.func_on_true(),
+            'on_false': self.func_on_false()
+        }
+        return s
+
+    def func_condition(self):
+
+        self.p.suspend()
+        print 'Suspending pipeline {} for 10 seconds'.format(self.p.name)  
+        sleep(10)
+        return True
+
+    def func_on_true(self):
+
+        print 'Resuming pipeline {}'.format(self.p.name) 
+        self.p.resume()
+
+    def func_on_false(self):
+        pass
+
+    def generate_pipeline(self):
+
+        self.p.add_stages(self.generate_stage())
+        return self.p
+
 
 
 if __name__ == '__main__':
 
-    # arguments for AppManager
+    # Create a dictionary describe four mandatory keys:
+    # resource, walltime, cores and project
+    # resource is 'local.localhost' to execute locally
+    res_dict = {
 
-
-    walltime = 30
-    num_hyperparameters = 5
-    initial_hparams = [(0,7)] 
-    final_hparams = list()
-    final_hparams += number_of_hyperparameters * [initial_hparams[0]]
-
-    # Pipelines, Stages, Tasks 
-
-    md_p = md_pipeline(name = 'MD_simulation_pipeline')
-    cvae_p = cvae_pipe(name = 'CVAE_pipeline')
-
-    md_s1 = md_stage(name = 'MD_simulation_long_dur')
-    md_s2 = md_stage(name = 'MD_simulation_short_dur')
-
-    cvae_train_s = cvae_train_stage(name = 'cvae_training_stage')
-    cvae_inference_s = cvae_inference_stage(name = 'cvae_inference_stage')
-
-
-    for i in range(28):
-
-        # execute BoT simulations for 6 ns (long duration)
-        # this will generate enough initial trajectory data for cvae training
-
-        t = md_task(name 'md_long_task_{}'.format(i), duration = 6000)
-
-        # Add the Task to the Stage
-
-        md_s1.add_tasks(t)
-
-
-    # Add Stage to the Pipeline
-
-    md_p.add_stages(md_s1)
-
-    logger.info('adding stage {} with {} tasks'.format(md_s1.name, md_s1._task_count))
-
-
-    for i in range(28):
-
-        # execute BoT simulations for 1 ns (short duration)
-        
-
-        t = md_task(name 'md_short_task_{}'.format(i), duration = 600)
-
-        # Add the Task to the Stage
-
-        md_s2.add_tasks(t)
-
-
-    # Add Stage to the Pipeline
-
-    md_p.add_stages(md_s2)
-
-    logger.info('adding stage {} with {} tasks'.format(md_s2.name, md_s2._task_count))
-
-
-    for i in range(len(hyperparameters)**2):
-        t = cvae_training_task(name = 'cvae_training_task', hyperparameters = final_hparams)  
-        
-
-
-    for i in range(2**len(final_hparams)): 
-    
-        # run Bayesian optimization in parallel
-        # each optimization runs for n_iterations
-
-        t = cvae_training_task(name = 'optimization_{}'.format(i), hyperspace_index = i)
-        
-        cvae_train_s.add_tasks(t)
-        
-    cvae_p.add_stages(cvae_train_s2)
-
+        'resource': 'local.localhost',
+        'walltime': 15,
+        'cpus': 2,
+    }
 
     # Create Application Manager
     appman = AppManager(hostname=hostname, port=port)
-
-    total_cpus = 2**len(final_hparams)*32 + 28*32
-
-    res_dict = {
-
-        'resource': 'xsede.bridges',
-        'project' : 'mc3bggp',
-        'queue' : 'GPU',
-        'walltime': walltime,
-        'cpus': total_cpus,
-        'access_schema': 'gsissh'
-    }
-
-    # Assign resource manager to the Application Manager
     appman.resource_desc = res_dict
-    appman.shared_data = []
 
-    
+    md_p = MD_pipeline() 
+    p = md_p.generate_pipeline()
+
+
     # Assign the workflow as a set of Pipelines to the Application Manager
-    appman.workflow = [md_p,cvae_p]
+    appman.workflow = [p]
 
     # Run the Application Manager
     appman.run()
 
-    
+
