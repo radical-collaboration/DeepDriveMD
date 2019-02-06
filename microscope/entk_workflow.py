99a359f5a26662ba868ea6dbd4530363931e18a3
Jumana Dakka <jumanadakka@gmail.com>
2019-02-06 14:35:46 -0500

    resource configs

--- microscope/entk_workflow.py
+++ microscope/entk_workflow.py
@@ -26,7 +26,7 @@ logger = ru.Logger(__name__, level='DEBUG')
 
 class MD_pipeline:
 
-    def __init__(self, name = 'MD_pipeline'):
+    def __init__(self, name = 'MD_pipeline', md_tasks = None):
 
         self.name = name
         self.engine = None # OpenMM
@@ -42,12 +42,90 @@ class MD_pipeline:
         self.duration = None 
         self.p = Pipeline()
         self.p.name = name
+        self.md_sim_tasks = md_tasks
         
+    @property
+    def md_gpus(self):
+        return self.gpu_processes * self.gpu_threads_per_process * self._md_sim_tasks
 
-    def generate_task(self):
+    def generate_task(self, md_task):
         
         task = Task()
-        task.name = 'md_task'
+        task.name = 'md_task_{}'.format(md_task)
+
+        task.pre_exec = ['export PATH=/home/dakka/stress-ng-0.09.40:$PATH']
+        task.executable = ['export PATH=/home/dakka/stress-ng-0.09.40:$PATH']
+        task.arguments = ['-c', '32', '-t', '{}'.format(self.duration)]
+        task.cpu_reqs = {'processes': self.processes,
+                         'process_type': 'MPI',
+                         'threads_per_process': self.threads_per_process,
+                         'thread_type': None
+                         }
+
+        task.gpu_reqs = {'processes': self.gpu_processes,
+                         'process_type': 'MPI',
+                         'threads_per_process': self.gpu_threads_per_process,
+                         'thread_type': None
+                         }
+
+        return task
+
+    def generate_stage(self):
+        s = Stage()
+        s.name = 'MD_stage'
+        s.add_tasks(self.generate_task(i) for i in range(self.md_sim_tasks)) 
+        s.post_exec = {
+            'condition': self.func_condition(),
+            'on_true': self.func_on_true(),
+            'on_false': self.func_on_false()
+        }
+        return s
+
+    def func_condition(self):
+
+        # self.p.suspend()
+        print 'Suspending pipeline {} for 5 seconds'.format(self.p.name)  
+        sleep(5)
+        return True
+
+    def func_on_true(self):
+
+        print 'Resuming pipeline {}'.format(self.p.name) 
+        # self.p.resume()
+
+    def func_on_false(self):
+        pass
+
+    def generate_pipeline(self):
+
+        self.p.add_stages(self.generate_stage())
+
+        return self.p
+
+class CVAE_pipeline:
+
+    def __init__(self, name = 'CVAE_pipeline', no_hyperparameters = None):
+
+        self.name = name
+        self.engine = None
+
+        self.processes = None
+        self.gpu_processes = None
+        self.threads_per_process = None
+        self.gpu_threads_per_process = None
+        self.duration = None 
+        self.p = Pipeline()
+        self.p.name = name
+        self.cvae_tasks = 2**no_hyperparameters
+    
+    @property
+    def cvae_gpus(self):
+        return self.gpu_processes * self.gpu_threads_per_process * self.cvae_tasks  
+
+    def generate_task(self, task_no):
+        
+        task = Task()
+        task.name = 'cvae_train_task'
 
         task.pre_exec = ['export PATH=/home/dakka/stress-ng-0.09.40:$PATH']
         task.executable = ['export PATH=/home/dakka/stress-ng-0.09.40:$PATH']
@@ -79,86 +157,15 @@ class MD_pipeline:
 
     def func_condition(self):
 
-        self.p.suspend()
+        # self.p.suspend()
         print 'Suspending pipeline {} for 10 seconds'.format(self.p.name)  
-        sleep(10)
+        sleep(5)
         return True
 
     def func_on_true(self):
 
         print 'Resuming pipeline {}'.format(self.p.name) 
-        self.p.resume()
-
-    def func_on_false(self):
-        pass
-
-    def generate_pipeline(self):
-
-        self.p.add_stages(self.generate_stage())
-
-        return self.p
-
-class CVAE_pipeline:
-
-    def __init__(self, name = 'CVAE_pipeline'):
-
-        self.name = name
-        self.engine = None
-        self.system = None
-
-        self.processes = None
-        self.gpu_processes = None
-        self.threads_per_process = None
-        self.gpu_threads_per_process = None
-        self.duration = None 
-        self.p = Pipeline()
-        self.p.name = name
-        
-
-    def generate_task(self, task_no):
-        
-        task = Task()
-        task.name = 'cvae_train_task_{}'.format(task_no)
-
-        task.pre_exec = ['export PATH=/home/dakka/stress-ng-0.09.40:$PATH']
-        task.executable = ['export PATH=/home/dakka/stress-ng-0.09.40:$PATH']
-        task.arguments = ['-c', '32', '-t', '{}'.format(self.duration)]
-        task.cpu_reqs = {'processes': self.processes,
-                         'process_type': 'MPI',
-                         'threads_per_process': self.threads_per_process,
-                         'thread_type': None
-                         }
-
-        task.gpu_reqs = {'processes': self.gpu_processes,
-                         'process_type': 'MPI',
-                         'threads_per_process': self.gpu_threads_per_process,
-                         'thread_type': None
-                         }
-
-        return task
-
-    def generate_stage(self):
-        s = Stage()
-        s.name = 'MD_stage'
-        s.add_tasks(self.generate_task(task_no = "i"))
-        s.post_exec = {
-            'condition': self.func_condition(),
-            'on_true': self.func_on_true(),
-            'on_false': self.func_on_false()
-        }
-        return s
-
-    def func_condition(self):
-
-        self.p.suspend()
-        print 'Suspending pipeline {} for 10 seconds'.format(self.p.name)  
-        sleep(10)
-        return True
-
-    def func_on_true(self):
-
-        print 'Resuming pipeline {}'.format(self.p.name) 
-        self.p.resume()
+        # self.p.resume()
 
     def func_on_false(self):
         pass
@@ -172,6 +179,30 @@ class CVAE_pipeline:
 
 if __name__ == '__main__':
 
+    # Create Application Manager
+    appman = AppManager(hostname=hostname, port=port)
+    appman.resource_desc = res_dict
+
+    # Create MD pipeline, specify number of simulations (md_tasks)
+    md_p = MD_pipeline() 
+    p1 = md_p.generate_pipeline(md_tasks = 4)
+
+    # Create CVAE pipeline, specify number of hyperparameters 
+    cvae_p = CVAE_pipeline() 
+    p2 = cvae_p.generate_pipeline(no_hyperparameters = 5)
+
+    # Specify the resource request
+    total_gpus = md_gpus + cvae_gpus
+
+    # On Bridges every 2 GPUs (P100) will land on 32 CPUs
+    total_cpus = (total_gpus/2)*32
+
+    pipelines = set()
+    pipelines.add(p1,p2)
+
+    # Assign the workflow as a set of Pipelines to the Application Manager
+    appman.workflow = [pipelines]
+
     # Create a dictionary describe four mandatory keys:
     # resource, walltime, cores and project
     # resource is 'local.localhost' to execute locally
@@ -182,12 +213,6 @@ if __name__ == '__main__':
     #     'cpus': 2,
     # }
 
-    hyperparams = 5 # typically 5
-    walltime = 30 
-    cpus_for_hyperspace = (2**hyperparams)*32 
-    cpus_for_md = 28*32
-    total_cpus = cpus_for_hyperspace + cpus_for_md
-
     res_dict = {
 
         'resource': 'xsede.bridges',
@@ -195,24 +220,10 @@ if __name__ == '__main__':
         'queue' : 'GPU',
         'walltime': walltime,
         'cpus': total_cpus,
+        'gpus': total_gpus, 
         'access_schema': 'gsissh'
     }
 
-
-    # Create Application Manager
-    appman = AppManager(hostname=hostname, port=port)
-    appman.resource_desc = res_dict
-
-    md_p = MD_pipeline() 
-    p1 = md_p.generate_pipeline()
-
-
-    cvae_p = CVAE_pipeline() 
-    p2 = cvae_p.generate_pipeline()
-
-
-    # Assign the workflow as a set of Pipelines to the Application Manager
-    appman.workflow = [p]
 
     # Run the Application Manager
     appman.run()
