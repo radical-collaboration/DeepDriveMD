993c230ff63f761311edabf6e6c711b6d7aec0a7
Ma, Heng <maheng1991@hotmail.com>
2019-03-06 15:18:11 -0500

    add argparse format to the simulation files

---
+++ microscope/experiments/MD_exps/exab/README.md
@@ -0,0 +1,6 @@
+# Run simulation 
+```bash 
+python run_openmm.py -f ./input_data/exab.gro -p input_data/exab.top
+python run_openmm.py -f ./input_data/exab.gro -p input_data/exab.top -c {check point file} 
+```
+
--- microscope/experiments/MD_exps/exab/run_openmm.py
+++ microscope/experiments/MD_exps/exab/run_openmm.py
@@ -1,17 +1,36 @@
 import simtk.unit as u
 import sys, os, shutil 
-import errno
+import argparse 
 
 sys.path.append('../')
-
 from MD_utils.openmm_simulation import openmm_simulate_charmm_npt_z 
 
-top_file = os.path.abspath('./input_data/exab.top')
-pdb_file = os.path.abspath('./input_data/exab.gro')
+# top_file = os.path.abspath('./input_data/exab.top')
+# pdb_file = os.path.abspath('./input_data/exab.gro')
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", help="pdb file")
+parser.add_argument("-p", help="topology file")
+parser.add_argument("-c", help="check point file to restart simulation")
+
+args = parser.parse_args()
+
+if args.f:
+    pdb_file = os.path.abspath(args.f)
+else:
+    raise Error("No pdb file assigned")
+
+if args.p:
+    top_file = os.path.abspath(args.p)
+else:
+    top_file = None
+
+if args.c:
+    check_point = os.path.abspath(args.c)
+else:
+    check_point = None
 
 gpu_index = 0
-check_point = None
-
 
 openmm_simulate_charmm_npt_z(top_file, pdb_file,
                            check_point = check_point,
