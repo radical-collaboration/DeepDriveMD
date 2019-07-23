d9106b2aed4ff9ada3ae99dfc805b9b6339be34a
Ma, Heng <mah1@ornl.gov>
2019-07-23 12:34:15 -0400

    split the stages into functions

--- microscope/experiments/summit_md.py
+++ microscope/experiments/summit_md.py
@@ -20,144 +20,192 @@ if os.environ.get('RADICAL_ENTK_VERBOSE') is None:
 # [1] https://www.olcf.ornl.gov/for-users/system-user-guides/summit/summit-user-guide/scheduling-policy
 # [2] https://docs.google.com/document/d/1XFgg4rlh7Y2nckH0fkiZTxfauadZn_zSn3sh51kNyKE/
 #
-# export RMQ_HOSTNAME=two.radical-project.org 
-# export RMQ_PORT=33235 
-# export RADICAL_PILOT_DBURL=mongodb://user:user@ds223760.mlab.com:23760/adaptivity 
-#
+'''
+export RMQ_HOSTNAME=two.radical-project.org 
+export RMQ_PORT=33235 
+export RADICAL_PILOT_DBURL=mongodb://user:user@ds223760.mlab.com:23760/adaptivity 
+'''
 #
 
+
+CUR_STAGE=0
+MAX_STAGE=4
 
 def generate_training_pipeline():
+    """
+    Function to generate the CVAE_MD pipeline
+    """
+
+    def generate_MD_stage(num_MD=1): 
+        """
+        Function to generate MD stage. 
+        """
+        s1 = Stage()
+        s1.name = 'MD'
+
+        # MD tasks
+        for i in range(num_MD):
+            t1 = Task()
+            # https://github.com/radical-collaboration/hyperspace/blob/MD/microscope/experiments/MD_exps/fs-pep/run_openmm.py
+            t1.pre_exec = [] 
+            t1.pre_exec += ['module load cuda/9.1.85']
+            t1.pre_exec += ['source activate omm'] 
+            t1.pre_exec += ['export PYTHONPATH=/gpfs/alpine/scratch/hm0/bip179/entk_test/hyperspace/microscope/experiments/MD_exps:$PYTHONPATH'] 
+            t1.pre_exec += ['cd /gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_exps/fs-pep'] 
+            time_stamp = int(time.time())
+            t1.pre_exec += ['mkdir -p omm_runs_%d && cd omm_runs_%d' % (time_stamp, time_stamp)]
+            t1.executable = ['/ccs/home/hm0/.conda/envs/omm/bin/python']  # run_openmm.py
+            t1.arguments = ['/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_exps/fs-pep/run_openmm.py', 
+                    '-f', '/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_exps/fs-pep/pdb/100-fs-peptide-400K.pdb']
+
+            t1.cpu_reqs = {'processes': 1,
+                              'threads_per_process': 4,
+                              'thread_type': 'OpenMP'
+                              }
+            t1.gpu_reqs = {'processes': 1,
+                              'threads_per_process': 1,
+                              'thread_type': 'CUDA'
+                             }
+                              
+            # Add the MD task to the simulating stage
+            s1.add_tasks(t1)
+            time.sleep(1) 
+        return s1 
+
+
+    def generate_aggregating_stage(): 
+        """ 
+        Function to concatenate the MD trajectory (h5 contact map) 
+        """ 
+        s2 = Stage()
+        s2.name = 'aggregating'
+
+        # Aggregation task
+        t2 = Task()
+        # https://github.com/radical-collaboration/hyperspace/blob/MD/microscope/experiments/MD_to_CVAE/MD_to_CVAE.py
+        t2.pre_exec = [] 
+        t2.pre_exec += ['source activate omm'] 
+        t2.pre_exec += ['cd /gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_to_CVAE']
+        t2.executable = ['/ccs/home/hm0/.conda/envs/omm/bin/python']  # MD_to_CVAE.py
+        t2.arguments = ['/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_to_CVAE/MD_to_CVAE.py', 
+                '-f', '/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_exps/fs-pep']
+
+        # Add the aggregation task to the aggreagating stage
+        s2.add_tasks(t2)
+        return s2 
+
+
+    def generate_ML_stage(num_ML=1): 
+        """
+        Function to generate the learning stage
+        """
+        s3 = Stage()
+        s3.name = 'learning'
+
+        # learn task
+        for i in range(4): 
+            t3 = Task()
+            # https://github.com/radical-collaboration/hyperspace/blob/MD/microscope/experiments/CVAE_exps/train_cvae.py
+            t3.pre_exec = []
+            t3.pre_exec += ['module load cuda/9.1.85']
+            t3.pre_exec += ['source activate omm'] 
+            t3.pre_exec += ['export PYTHONPATH=/gpfs/alpine/scratch/hm0/bip179/entk_test/hyperspace/microscope/experiments/CVAE_exps:$PYTHONPATH']
+            t3.pre_exec += ['cd /gpfs/alpine/scratch/hm0/bip179/entk_test/hyperspace/microscope/experiments/CVAE_exps']
+            time_stamp = int(time.time())
+            dim = i + 3 
+            cvae_dir = 'cvae_runs_%.2d_%d' % (dim, time_stamp) 
+            t3.pre_exec += ['mkdir -p {0} && cd {0}'.format(cvae_dir)]
+            t3.executable = ['/ccs/home/hm0/.conda/envs/omm/bin/python']  # train_cvae.py
+            t3.arguments = ['/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/CVAE_exps/train_cvae.py', 
+                    '-f', '/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_to_CVAE/cvae_input.h5', 
+                    '-d', dim] 
+            
+            t3.cpu_reqs = {'processes': 1,
+                    'threads_per_process': 4,
+                    'thread_type': 'OpenMP'
+                    }
+            t3.gpu_reqs = {'processes': 1,
+                    'threads_per_process': 1,
+                    'thread_type': 'CUDA'
+                    }
+        
+            # Add the learn task to the learning stage
+            s3.add_tasks(t3)
+            time.sleep(1) 
+        return s3 
+
+
+    def generate_interfacing_stage(): 
+        s4 = Stage()
+        s4.name = 'scanning'
+
+        # Scaning for outliers and prepare the next stage of MDs 
+        t4 = Task() 
+        t4.pre_exec = [] 
+        t4.pre_exec += ['module load cuda/9.1.85'] 
+        t4.pre_exec += ['source activate omm'] 
+        t4.pre_exec += ['export PYTHONPATH=/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/CVAE_exps:$PYTHONPATH'] 
+        t4.pre_exec += ['cd /gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/Outlier_search'] 
+        # python outlier_locator.py -m ../MD_exps/fs-pep -c ../CVAE_exps -p ../MD_exps/fs-pep/pdb/100-fs-peptide-400K.pdb 
+        t4.executable = ['/ccs/home/hm0/.conda/envs/omm/bin/python'] 
+        t4.arguments = ['outlier_locator.py', '-m', '../MD_exps/fs-pep', '-c', '../CVAE_exps -p', '../MD_exps/fs-pep/pdb/100-fs-peptide-400K.pdb']
+    #     t4.arguments = ['/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/Outlier_search/outlier_locator.py', 
+    #             '-m', '/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_exps/fs-pep', 
+    #             '-c', '/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/CVAE_exps', 
+    #             '-p', '/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_exps/fs-pep/pdb/100-fs-peptide-400K.pdb'
+    #             ]
+
+        t4.cpu_reqs = {'processes': 1,
+                'threads_per_process': 12,
+                'thread_type': 'OpenMP'
+                }
+        t4.gpu_reqs = {'processes': 1,
+                'threads_per_process': 1,
+                'thread_type': 'CUDA'
+                }
+        s4.add_tasks(t4) 
+        
+        return s4
+
+
+    def func_condition(): 
+        global CUR_STAGE, MAX_STAGE 
+        if CUR_STAGE <= MAX_STAGE: 
+            func_on_ture()
+        func_on_false()
+
+    def func_on_ture(): 
+        pass 
+
+    def func_on_false(): 
+        print 'Done' 
+
+
 
     p = Pipeline()
     p.name = 'MD_ML'
 
     # --------------------------
     # MD stage
-    s1 = Stage()
-    s1.name = 'MD'
-
-    # MD tasks
-    for i in range(12):
-        t1 = Task()
-        # https://github.com/radical-collaboration/hyperspace/blob/MD/microscope/experiments/MD_exps/fs-pep/run_openmm.py
-        t1.pre_exec = [] 
-        t1.pre_exec += ['module load cuda/9.1.85']
-        t1.pre_exec += ['source activate omm'] 
-        t1.pre_exec += ['export PYTHONPATH=/gpfs/alpine/scratch/hm0/bip179/entk_test/hyperspace/microscope/experiments/MD_exps:$PYTHONPATH'] 
-        t1.pre_exec += ['cd /gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_exps/fs-pep'] 
-        time_stamp = int(time.time())
-        t1.pre_exec += ['mkdir -p omm_runs_%d && cd omm_runs_%d' % (time_stamp, time_stamp)]
-        t1.executable = ['/ccs/home/hm0/.conda/envs/omm/bin/python']  # run_openmm.py
-        t1.arguments = ['/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_exps/fs-pep/run_openmm.py', 
-                '-f', '/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_exps/fs-pep/pdb/100-fs-peptide-400K.pdb']
-
-        t1.cpu_reqs = {'processes': 1,
-                          'threads_per_process': 4,
-                          'thread_type': 'OpenMP'
-                          }
-        t1.gpu_reqs = {'processes': 1,
-                          'threads_per_process': 1,
-                          'thread_type': 'CUDA'
-                         }
-                          
-        # Add the MD task to the simulating stage
-        s1.add_tasks(t1)
-        time.sleep(1) 
-
+    s1 = generate_MD_stage(num_MD=12)
     # Add simulating stage to the training pipeline
-#     p.add_stages(s1)
+    p.add_stages(s1)
 
     # --------------------------
     # Aggregate stage
-    s2 = Stage()
-    s2.name = 'aggregating'
-
-    # Aggregation task
-    t2 = Task()
-    # https://github.com/radical-collaboration/hyperspace/blob/MD/microscope/experiments/MD_to_CVAE/MD_to_CVAE.py
-    t2.pre_exec = [] 
-    t2.pre_exec += ['source activate omm'] 
-    t2.pre_exec += ['cd /gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_to_CVAE']
-    t2.executable = ['/ccs/home/hm0/.conda/envs/omm/bin/python']  # MD_to_CVAE.py
-    t2.arguments = ['/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_to_CVAE/MD_to_CVAE.py', 
-            '-f', '/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_exps/fs-pep']
-
-    # Add the aggregation task to the aggreagating stage
-    s2.add_tasks(t2)
-
+    s2 = generate_aggregating_stage() 
     # Add the aggregating stage to the training pipeline
-#     p.add_stages(s2)
+    p.add_stages(s2)
 
     # --------------------------
     # Learning stage
-    s3 = Stage()
-    s3.name = 'learning'
-
-    # learn task
-    for i in range(4): 
-        t3 = Task()
-        # https://github.com/radical-collaboration/hyperspace/blob/MD/microscope/experiments/CVAE_exps/train_cvae.py
-        t3.pre_exec = []
-        t3.pre_exec += ['module load cuda/9.1.85']
-        t3.pre_exec += ['source activate omm'] 
-        t3.pre_exec += ['export PYTHONPATH=/gpfs/alpine/scratch/hm0/bip179/entk_test/hyperspace/microscope/experiments/CVAE_exps:$PYTHONPATH']
-        t3.pre_exec += ['cd /gpfs/alpine/scratch/hm0/bip179/entk_test/hyperspace/microscope/experiments/CVAE_exps']
-        time_stamp = int(time.time())
-        dim = i + 3 
-        cvae_dir = 'cvae_runs_%.2d_%d' % (dim, time_stamp) 
-        t3.pre_exec += ['mkdir -p {0} && cd {0}'.format(cvae_dir)]
-        t3.executable = ['/ccs/home/hm0/.conda/envs/omm/bin/python']  # train_cvae.py
-        t3.arguments = ['/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/CVAE_exps/train_cvae.py', 
-                '-f', '/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_to_CVAE/cvae_input.h5', 
-                '-d', dim] 
-        
-        t3.cpu_reqs = {'processes': 1,
-                'threads_per_process': 4,
-                'thread_type': 'OpenMP'
-                }
-        t3.gpu_reqs = {'processes': 1,
-                'threads_per_process': 1,
-                'thread_type': 'CUDA'
-                }
-    
-        # Add the learn task to the learning stage
-        s3.add_tasks(t3)
-        time.sleep(1) 
-
+    s3 = generate_ML_stage(num_ML=4) 
     # Add the learning stage to the pipeline
-#     p.add_stages(s3)
+    p.add_stages(s3)
 
     # --------------------------
     # Outlier identification stage
-    s4 = Stage()
-    s4.name = 'scanning'
-
-    # Scaning for outliers and prepare the next stage of MDs 
-    t4 = Task() 
-    t4.pre_exec = [] 
-    t4.pre_exec += ['module load cuda/9.1.85'] 
-    t4.pre_exec += ['source activate omm'] 
-    t4.pre_exec += ['export PYTHONPATH=/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/CVAE_exps:$PYTHONPATH'] 
-    t4.pre_exec += ['cd /gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/Outlier_search'] 
-    # python outlier_locator.py -m ../MD_exps/fs-pep -c ../CVAE_exps -p ../MD_exps/fs-pep/pdb/100-fs-peptide-400K.pdb 
-    t4.executable = ['/ccs/home/hm0/.conda/envs/omm/bin/python'] 
-    t4.arguments = ['outlier_locator.py', '-m', '../MD_exps/fs-pep', '-c', '../CVAE_exps -p', '../MD_exps/fs-pep/pdb/100-fs-peptide-400K.pdb']
-#     t4.arguments = ['/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/Outlier_search/outlier_locator.py', 
-#             '-m', '/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_exps/fs-pep', 
-#             '-c', '/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/CVAE_exps', 
-#             '-p', '/gpfs/alpine/bip179/scratch/hm0/entk_test/hyperspace/microscope/experiments/MD_exps/fs-pep/pdb/100-fs-peptide-400K.pdb'
-#             ]
-
-    t4.cpu_reqs = {'processes': 1,
-            'threads_per_process': 12,
-            'thread_type': 'OpenMP'
-            }
-    t4.gpu_reqs = {'processes': 1,
-            'threads_per_process': 1,
-            'thread_type': 'CUDA'
-            }
-    s4.add_tasks(t4) 
+    s4 = generate_interfacing_stage() 
     p.add_stages(s4) 
 
 
