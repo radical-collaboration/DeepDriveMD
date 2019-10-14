1c114e6c7fc0d72ff4e309736efb2eebc1c3837a
yngtodd <young.todd.mk@gmail.com>
2019-10-14 10:54:38 -0400

    conveniently saving results

--- microscope/experiments/pre_run.py
+++ microscope/experiments/pre_run.py
@@ -1,21 +1,24 @@
 import os, glob 
 import shutil
-
+import time
 
 omm_dirs = glob.glob('MD_exps/fs-pep/omm_runs*') 
 cvae_dirs = glob.glob('CVAE_exps/cvae_runs_*') 
 jsons = glob.glob('Outlier_search/*json') 
 
+result_save = os.path.join('./results', 'result_%d' % int(time.time())) 
+os.makedirs(result_save) 
+
 for omm_dir in omm_dirs: 
-    shutil.move(omm_dir, 'MD_exps/fs-pep/old_MDs/') 
+    shutil.move(omm_dir, result_save) 
 
 for cvae_dir in cvae_dirs: 
-    shutil.rmtree(cvae_dir) 
+    shutil.move(cvae_dir, result_save) 
 
 for json in jsons: 
-    os.remove(json) 
+    shutil.move(json, result_save) 
 
 if os.path.isdir('Outlier_search/outlier_pdbs'): 
-    shutil.rmtree('Outlier_search/outlier_pdbs') 
+    shutil.move('Outlier_search/outlier_pdbs', result_save) 
 
 
