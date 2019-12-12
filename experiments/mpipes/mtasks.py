828073cf1bd23300cfa5f2ed01c9693b228189a6
Hyungro Lee <hroe.lee@gmail.com>
2019-12-12 16:26:03 -0500

    analytics by download=True

--- experiments/mpipes/mtasks.py
+++ experiments/mpipes/mtasks.py
@@ -15,8 +15,15 @@ def gen_units(loop_count=1, exec_second=10):
     cuds = []
     for i in range(int(loop_count)):
         cud = rp.ComputeUnitDescription()
-        cud.executable    = '/bin/sleep {}'.format(exec_second)
+        cud.pre_exec = ["date"]
+        cud.pre_exec += ["echo 'for i in $(seq {}); do /usr/bin/sleep 1; date; done' > a.sh".format(exec_second)]
+        cud.executable = 'bash a.sh' 
+        cud.post_exec = "date"
+        cud.cpu_threads = 4
         cud.cpu_processes = 1
+        cud.cpu_thread_type = "OpenMP"
+        cud.gpu_processes = 0
+        cud.gpu_thread_type = "CUDA"
         cuds.append(cud)
     return cuds
 
@@ -33,16 +40,21 @@ if __name__ == '__main__':
 
     rp_resource_description = {
             'resource'      : "ornl.summit",
-            'runtime'       : 30,  # pilot runtime (min)
+            'runtime'       : 20,  # pilot runtime (min)
             'exit_on_error' : True,
-            'project'       : "CSC393",
+            'project'       : "LRN005",
             'queue'         : "batch",
             'access_schema' : "local",
-            'cores'         : 168
+            'cores'         : 168*3,
             }
 
     session, _, umgr = prepare_pilot(rp_resource_description)
-    cuds = set_ratio({"1:1:1": "10,100,1000"})
+    #cuds = set_ratio({"40:40:40": "10,100,1000"})
+    #cuds = set_ratio({"58:31:31": "10,100,1000"})
+    #cuds = set_ratio({"76:22:22": "10,100,1000"})
+    #cuds = set_ratio({"94:13:13": "10,100,1000"})
+    #cuds = set_ratio({"112:4:4": "10,100,1000"})
+    cuds = set_ratio({"4:4:112": "10,100,1000"})
     umgr.submit_units(cuds)
     umgr.wait_units()
-    session.close()
+    session.close(download=True)
