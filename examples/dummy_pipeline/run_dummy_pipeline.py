a944dc749c6da11f7b80c1d555aaa7bf2e27b6f3
Mariya Goliyad <goliyad@br013.ib.bridges2.psc.edu>
2025-11-22 23:56:22 -0500

    dargon test

--- examples/dummy_pipeline/run_dummy_pipeline.py
+++ examples/dummy_pipeline/run_dummy_pipeline.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import asyncio
 from radical.asyncflow import WorkflowEngine
-from radical.asyncflow import ConcurrentExecutionBackend
+from radical.asyncflow import DragonExecutionBackend
 from concurrent.futures import ProcessPoolExecutor
 from radical.asyncflow import RadicalExecutionBackend
 from ddmd import DummyWorkflow
@@ -10,25 +10,9 @@ SIM_CORES = 3
 TRAIN_CORE = 1
 TOTAL_CORES= SIM_CORES + TRAIN_CORE
 
-RESOURCES = {
-            'runtime': 30, 
-            'resource': 'local.localhost', 
-            #'resource': 'purdue.anvil',
-            'cores': TOTAL_CORES
-        }
-
-raptor_config = {
-    "masters": [{
-        "ranks": 1,
-        "workers": [{
-            "ranks": TRAIN_CORE
-        }]
-    }]
-}
-
 async def run_ddmd():
 
-    engine = await ConcurrentExecutionBackend(ProcessPoolExecutor())
+    engine = await DragonExecutionBackend()
     #engine = await RadicalExecutionBackend(RESOURCES, raptor_config)
 
     # Create the async workflow engine
